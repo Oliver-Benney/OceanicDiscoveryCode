8c0d2a663620a10891ab1415601ec254d4b344e9
Oliver-Benney <93660106+Oliver-Benney@users.noreply.github.com>
2024-04-23 16:08:52 +0100

    Add files via upload

---
+++ P5ToFirebase/p5FIrebase/js/firebase.js
@@ -0,0 +1,12 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyAxJPn3rJUv8K-6v12FlI7uBNfYf8yIW6w",
+  authDomain: "oceanicdiscovery-8a3aa.firebaseapp.com",
+  databaseURL: "https://oceanicdiscovery-8a3aa-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "oceanicdiscovery-8a3aa",
+  storageBucket: "oceanicdiscovery-8a3aa.appspot.com",
+  messagingSenderId: "197295999629",
+  appId: "1:197295999629:web:3a3ced3ec40ae3172870c4",
+  measurementId: "G-G328XJN18W"
+});
+
+var db = firebase.firestore()
---
+++ P5ToFirebase/p5FIrebase/js/script.js
@@ -0,0 +1,64 @@
+// Create a reference to the 'count' document
+const countRef = db.collection('people').doc('count');
+
+// Set up a listener for real-time updates
+countRef.onSnapshot((doc) => {
+  if (doc.exists) {
+    const newData = doc.data();
+    const newCount = newData.count;
+  }
+});
+
+
+let video;
+let poseNet;
+let poses = [];
+let peopleCount = 0;
+let lastCountTime = 0;
+const countInterval = 15000; // 15 seconds in milliseconds
+
+function setup() {
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.size(width, height);
+
+  // Create a new poseNet method with a single detection
+  poseNet = ml5.poseNet(video, { detectionType: 'multi', architecture: 'ResNet50', outputStride: 32, maxPoseDetections: 10, scoreThreshold: 0.7 }, modelLoaded);
+  poseNet.on('pose', function(results) {
+    poses = results;
+  });
+
+  video.hide();
+
+  // Call countPeople() on setup to initialize peopleCount
+  countPeople();
+}
+
+function modelLoaded() {
+  console.log('Model Loaded!');
+}
+
+function countPeople() {
+  peopleCount = poses.length;
+
+  // Update Firebase Firestore with the new count
+  countRef.set({
+    count: peopleCount
+  })
+}
+
+function draw() {
+  image(video, 0, 0, width, height);
+
+  // Update the count once every five seconds
+  if (millis() - lastCountTime >= countInterval) {
+    countPeople();
+    lastCountTime = millis();
+  }
+
+  // Display the number of people detected
+  fill(255);
+  textSize(20);
+  text('People Count: ' + peopleCount, 10, 30);
+  console.log(peopleCount);
+}
